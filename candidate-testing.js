5d851389228140d2ae94c6a235e002561316b2b2
Harshala Sadhale <harshala2018@gmail.com>
2021-02-16 22:28:17 +0000

    Another version

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,13 +9,11 @@ let candidateAnswer = "";
 let questions = ["Who was the first American woman in space?","True or false: 5000 meters = 5 kilometers.","(5 + 3)/2 * 10 = ?","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?","What is the minimum crew size for the ISS?"];
 let correctAnswers = ["Sally Ride","True","40","Trajectory","3"];
 let candidateAnswers = [];
-//Names
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
   candidateName = input.question("What is the name of the Candidate? ");
 console.log("Candidate Name: "+ candidateName);
 }
-// Asking Questions to Candidates
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 for(let i=0; i<questions.length;i++){
@@ -27,7 +25,6 @@ for(let i=0; i<questions.length;i++){
   candidateAnswers.push();  
  }
 }
-//Checking and Grading
 function gradeQuiz(candidateAnswers) {
  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
   let grade; let answerCount = 0;
